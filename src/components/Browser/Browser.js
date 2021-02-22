920d7673ec1796a441cdf354c5dc0870f9bfbfb5
Adam <aiskrzycki01@gmail.com>
2021-02-22 20:43:35 +0100

    Added some Galery component improvements

--- src/components/Browser/Browser.js
+++ src/components/Browser/Browser.js
@@ -2,39 +2,77 @@ import React, { useEffect } from 'react';
 import "./Browser.css";
 import { useState } from 'react';
 import BrowserInput from '../BrowserInput/BrowserInput';
-import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 const Browser = () => {
 
     const [inputValue, setInputValue] = useState("");
-    const [isSearching, setIsSearching] = useState(false);
-    const [photos, setPhotos] = useState([]);
+    const [hints, setHints] = useState([]);
+
+    const history = useHistory()
 
     const onInputChange = (e) => {
-        if (inputValue.length >= 2) {
-            console.log("No prompts found")
-        }
         setInputValue(e.target.value)
     }
 
     const handleKeyPress = (e) => {
-        if(inputValue.length !== 0) {    
-            if (e.code === "Enter") {
-                setIsSearching(true);
-            }
+
+        if (e.code === "Enter") {
+            goToGalery(null);
         }
+
     }
+
+    const goToGalery = (e) => {
+            if(e === null) {
+            history.push(`/galery/${inputValue}`)
+            } else history.push(`/galery/${document.getElementById("Hint").innerHTML}`)  // FIX e.target.value for paragraph
+        
+    }
+
+    useEffect(() => {
+        if (inputValue.length >= 2) {
+            const fetchData = async () => {
+                try {
+                    const response = await fetch(`https://api.unsplash.com/search?query=${inputValue}&client_id=RIvvLcDMXmoibV0w0qpbOnwDWWWNeh5YuomXUrbgsuQ`)
+                    const data = await response.json();
+                    setHints(data.photos.results);
+                }
+                catch (err) {
+                    console.log(err)
+                }
+            }
+
+            fetchData();
+        }
+    }, [inputValue])
+
+    //TODO
+    //Galery component - photo preview (modal)
+    //Galery component - fix searchbar
+    //Browser component - fix scrollbar when there is a lot of hints rendered
+    //General styling
 
 
     return (
         <>
-            {isSearching ? <Redirect to={`/galery/${inputValue}`} /> : null}
             <div className="BrowserContainer">
                 <section className="UnsplashContainer">
                     <header className="UnsplashHeader">UNSPLASH</header>
                     <p>The internet source of freely-usable images</p>
                     <p>Powered by creators everywhere</p>
                     <BrowserInput value={inputValue} changed={onInputChange} onPress={handleKeyPress} />
+                    {inputValue ? <div className="HintsContainer">
+                        {hints.map(hint => {
+                            return <p id="Hint" onClick={goToGalery}>{hint.alt_description}</p>
+                    })}
+                    </div> : null}
+                    {/* <select onChange={goToGalery} className="Dropdown">
+                        <option>Select One</option>
+                        {hints.map(hint => {
+                            return <option value={hint.alt_description}>{hint.alt_description}</option>
+                        })}
+                    </select> */}
                 </section>
             </div>
         </>
--- src/components/Galery/Galery.js
+++ src/components/Galery/Galery.js
@@ -2,38 +2,70 @@ import React, { useEffect, useState } from 'react';
 import BrowserInput from '../BrowserInput/BrowserInput';
 import './Galery.css';
 import { useParams } from 'react-router-dom'
+import { Dialog, DialogTitle, DialogContent } from "@material-ui/core";
 
 
 const Galery = () => {
 
     const { searchTag } = useParams()
     const [photos, setPhotos] = useState([]);
+    const [relatedSearches, setRelatedSearches] = useState();
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(`https://api.unsplash.com/search?query=${searchTag}&client_id=ToDo`)
+                const response = await fetch(`https://api.unsplash.com/search?query=${searchTag}&client_id=RIvvLcDMXmoibV0w0qpbOnwDWWWNeh5YuomXUrbgsuQ`)
                 const data = await response.json();
-                console.log("data: ", data)
+                console.log("data: ", data);
                 setPhotos(data.photos.results);
-                console.log("photos: ", photos)
+                setRelatedSearches(data.related_searches)
             }
             catch (err) {
                 console.log(err)
             }
         }
 
+
         fetchData();
     }, [searchTag])
 
-    return(
+    const handleModalOpen = () => {
+        setOpenModal(true);
+    }
+
+    const handleModalClose = () => {
+        setOpenModal(false);
+    }
+
+    return (
         <nav className="NavBar">
-            {searchTag ? <span>Your search tag is {searchTag}</span> : <span>no searchtag provided</span>}
-            <BrowserInput />
+            <Dialog open={openModal} onClose={handleModalClose}>
+                <DialogTitle>TEST TITLE</DialogTitle>
+                <DialogContent>TEST CONTENT</DialogContent>
+            </Dialog>
+            <BrowserInput value={searchTag} />
+            {searchTag ? <p className="SearchTag">{searchTag}</p> : <p>no searchtag provided</p>}
             <div>
-                {photos.map(photo => {
-                    return <img src={photo.urls.small}></img>
-                })}
+                <div className="RelatedSearchesContainer">
+                    {relatedSearches ? relatedSearches.map(search => {
+                        return <div className="RelatedSearch">{search.title}</div>
+                    }) : null}
+                </div>
+                <div className="GaleryGrid">
+                    {photos.map(photo => {
+                        return (
+                            <div key={photo.id}>
+                                <img className="Image" src={photo.urls.small} onClick={handleModalOpen}></img>
+                                <div className="IndividualTagsContainer">
+                                    {photo.tags.map(tag => {
+                                        return <p className="IndividualTag">{tag.title}</p>
+                                    })}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </nav>
     )
